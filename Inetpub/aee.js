8bd8014fcdfa703582f5ec2de43eea17d9155bb0
Hocza József Szabolcs <dpm.1@fenysoft.com>
2020-02-01 00:47:18 +0100

    AJAX hívás tesztelése + AEE-s JavaScript

---
+++ Inetpub/aee.js
@@ -0,0 +1,56 @@
+    function DailyOpen(ACash)
+		{
+      var xhttp = new XMLHttpRequest();
+      
+			xhttp.onreadystatechange = function()
+			{
+        if (this.readyState == 4)
+				{
+          document.getElementById("Status").innerHTML =
+          this.responseText;
+        }
+      };
+			
+			var sessionId = document.getElementById("SessionId").Value;
+      xhttp.open("POST", "DailyOpen", true);
+			xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xhttp.send("SessionId=" + sessionId + "&DocumentGeneral.Barcode=auto&DocumentGeneral.CopyCount=1&Payments[0].Amount=" + ACash + "&Payments[0].PaymentMediaIndex=0&Payments[0].Info=Befizetés");
+    }
+
+    function CashReport()
+		{
+      var xhttp = new XMLHttpRequest();
+      
+			xhttp.onreadystatechange = function()
+			{
+        if (this.readyState == 4)
+				{
+          document.getElementById("Status").innerHTML =
+          this.responseText;
+        }
+      };
+			
+			var sessionId = document.getElementById("SessionId").Value;
+      xhttp.open("POST", "CashReport", true);
+			xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xhttp.send("SessionId=" + sessionId + "&DocumentGeneral.Barcode=auto&DocumentGeneral.CopyCount=1");
+    }
+		
+    function DailyClose()
+		{
+      var xhttp = new XMLHttpRequest();
+      
+			xhttp.onreadystatechange = function()
+			{
+        if (this.readyState == 4)
+				{
+          document.getElementById("Status").innerHTML =
+          this.responseText;
+        }
+      };
+			
+			var sessionId = document.getElementById("SessionId").Value;
+      xhttp.open("POST", "DailyClose", true);
+			xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xhttp.send("SessionId=" + sessionId + "&DocumentGeneral.Barcode=auto&DocumentGeneral.CopyCount=1");
+    }
